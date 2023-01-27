359fce9b4b40943fb12156cb762aad4d83dca526
Ioannis Merianos <ioannismeri@gmail.com>
2023-01-27 04:10:15 +0200

    select component variants

--- ds.e/packages/react/src/molecules/Select/Select.stories.tsx
+++ ds.e/packages/react/src/molecules/Select/Select.stories.tsx
@@ -11,7 +11,22 @@ const options = [
 ];
 
 export default {
-  title: "Select",
+  title: "Molecules|Select",
 };
 
 export const Common = () => <Select options={options} />;
+
+export const RenderOption = () => (
+  <Select
+    options={options}
+    renderOption={({ getOptionRecommendedProps, option, isSelected }) => (
+      <span {...getOptionRecommendedProps()}>
+        {option.label} {isSelected ? "SELECTED !" : ""}
+      </span>
+    )}
+  />
+);
+
+export const CustomLabel = () => (
+  <Select label="Select a color" options={options} />
+);
